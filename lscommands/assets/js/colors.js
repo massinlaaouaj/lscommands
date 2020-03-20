36fde73a479e7b3ded7ecc397e60294f3c0295ee
massinlaaouaj <massin.laaouaj@gmail.com>
2020-03-20 17:03:08 +0100

    Change8

--- lscommands/assets/js/colors.js
+++
@@ -1,8 +0,0 @@
-function background(color) {
-    document.bgColor = color;
-    if (color=='black') {
-        document.body.style.color='white';
-    } else {
-        document.body.style.color='black';        
-    }
-}
